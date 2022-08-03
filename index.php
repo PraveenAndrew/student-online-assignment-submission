7a568ee2a4cec1a1300979565cdf53d7ef33ba76
PraveenAndrew <84122399+PraveenAndrew@users.noreply.github.com>
2022-08-03 15:58:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include_once "../resources/sweetalert.php";  ?>
+
+        <header>
+               <div class="logo">
+                      <img src="../img/logo.png" alt="ServerPrblm" title="spiher">
+                      <h2><strong>spiher</strong></h2>
+               </div>
+               <div id="menu-bar" class="fa-solid fa-bars"></div>
+               <div class="navbar">
+                      <ul>
+                             <li><a href="http://localhost/online-assignment-submission/students/index.php">Home</a></li>
+                             <li><a href="http://localhost/online-assignment-submission/students/register.php">Register</a></li>
+                             <li><a href="http://localhost/online-assignment-submission/students/login.php">Login</a></li>
+                             <li><a href="http://localhost/online-assignment-submission/admin/adminLogin.php">Admin</a></li>
+                      </ul>
+               </div>
+        </header>
+        
+
+        <section class="container-img">
+               <img src="../img/books.jpeg" alt="">
+        </section>
+
+        <section class="quotes">
+                    <marquee behavior="scroll" direction="up" scrollamount="1">😀 Focus on your goal 😀</marquee>
+                    <marquee behavior="scroll" direction="up" scrollamount="1">😎 Keep going  😎</marquee>
+                    <marquee behavior="scroll" direction="up" scrollamount="1">😀 Hard Work 😀</marquee>
+                    <marquee behavior="scroll" direction="up" scrollamount="1">😀 Smile 😀</marquee>
+                    <marquee behavior="scroll" direction="up" scrollamount="1">😀 Obey Your Parents 😀</marquee>
+        </section>
+
+       <?php include_once "../partials/footer.php"; ?>
+
+        <script src="./js/scripts.js"></script>
+
+</body>
+</html>
