a5c39b1426cbe1c3fd4eebaad9134bfe01e132db
PraveenAndrew <84122399+PraveenAndrew@users.noreply.github.com>
2022-08-03 16:01:56 +0530

    Add files via upload

---
+++ admin/resources/sweetalert.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title></title>
+        <!-- custom style -->
+        <link rel="stylesheet" href="../students/css/style.css"> 
+       <!-- font awersome -->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+       <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body>
